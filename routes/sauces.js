29b45ca48fc455b3ea73cc94739e29372c164131
Matisse Goby <genesissssss@gmail.com>
2022-02-03 21:24:32 +0100

    simplify  imports

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const likeCtrl = require('../controllers/like');
-const createSauceCtrl = require('../controllers/sauce/create');
-const deleteSauceCtrl = require('../controllers/sauce/delete');
-const getAllSauceCtrl = require('../controllers/sauce/getAll');
-const getOneSauceCtrl = require('../controllers/sauce/getOne');
-const modifySauceCtrl = require('../controllers/sauce/modify');
+const { likeSauce } = require('../controllers/like');
+const { createSauce } = require('../controllers/sauce/create');
+const { deleteSauce } = require('../controllers/sauce/delete');
+const { getAllSauce } = require('../controllers/sauce/getAll');
+const { getOneSauce } = require('../controllers/sauce/getOne');
+const { modifySauce } = require('../controllers/sauce/modify');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.post('/', auth, multer, createSauceCtrl.createSauce);
-router.delete('/:id', auth, deleteSauceCtrl.deleteSauce);
-router.get('/', auth, getAllSauceCtrl.getAllSauce);
-router.get('/:id', auth, getOneSauceCtrl.getOneSauce);
-router.put('/:id', auth, multer, modifySauceCtrl.modifySauce);
-router.post('/:id/like', auth, likeCtrl.likeSauce);
+router.post('/', auth, multer, createSauce);
+router.delete('/:id', auth, deleteSauce);
+router.get('/', auth, getAllSauce);
+router.get('/:id', auth, getOneSauce);
+router.put('/:id', auth, multer, modifySauce);
+router.post('/:id/like', auth, likeSauce);
 
 module.exports = router;
