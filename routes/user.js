69d2c4d8b8ab59d45ccb14bce960c360a1887ad0
Matisse Goby <genesissssss@gmail.com>
2022-02-03 22:06:03 +0100

    add password-validator

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const pwd = require('../middleware/password');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', pwd, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
